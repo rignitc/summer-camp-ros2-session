e840680145980ef2ab9c39f1adfc3eeef7f1048a
CasCard <abelcheruvathoor@gmail.com>
2022-05-22 21:53:55 +0530

    added simple service client example

---
+++ src/simple_service_client/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'simple_service_client'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abel',
+    maintainer_email='abelcheruvathoor@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'service = simple_service_client.service:main',
+            'client = simple_service_client.client:main',
+        ],
+    },
+)
